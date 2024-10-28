1d050287a1278db42bea0176e0b870a724aced45
pcs <peichunshu@163.com>
2024-10-28 22:47:50 +0800

    Change the home page

--- youthol-ui/src/router/index.js
+++ youthol-ui/src/router/index.js
@@ -1,13 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Index from '../views/index.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'index',
-      component: Index
+      name: 'home',
+      components: {
+        MainComponment: () => import('../views/index.vue')
+      }
     }
   ]
 })
